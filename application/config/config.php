498bcca2c0f9f9ef0d9ed7c665f9024798d1a3cb
andika1804 <rizkytaufan55@gmail.com>
2024-01-23 14:25:08 +0700

    tentara

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 // $config['base_url'] = 'http://localhost/DATATENTARACI3/index.php';
-$config['base_url'] = 'http://topek.xyz//index.php';
+$config['base_url'] = 'http://topek.xyz/';
 
 /*
 |--------------------------------------------------------------------------
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -29,7 +29,7 @@
   <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
     <div class="navbar-nav">
       <a class="nav-item nav-link" href="<?= base_url(); ?>">Home <span class="sr-only">(current)</span></a>
-      <a class="nav-item nav-link" href="<?= base_url(); ?>Tentara">Tentara</a>
+      <a class="nav-item nav-link" href="<?= base_url('index.php/'); ?>Tentara">Tentara</a>
       <a class="nav-item nav-link" href="#">About</a>
     </div>
   </div>
